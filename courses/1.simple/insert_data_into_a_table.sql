7ca51a3f565b69b54c90699b9fb247cf01c52409
Yuzuru Nishimiya <sonyalamsyah.learn@gmail.com>
2022-03-11 22:54:36 +0700

    course 1 init

---
+++ courses/1.simple/insert_data_into_a_table.sql
@@ -0,0 +1,9 @@
+INSERT INTO cities (name, country, population, area)
+VALUES ('Tokyo', 'Japan', 38505000, 8223);
+
+-- add multiple row in one statement
+INSERT INTO cities (name, country, population, area)
+VALUES
+    ('Delhi', 'India', 2812500, 2240),
+    ('Shanghai', 'China', 22125000, 4015),
+    ('San Paulo', 'Brazil', 20935000, 3043);
---
+++ courses/1.simple/retrieving_data_with_select.sql
@@ -0,0 +1,6 @@
+SELECT * FROM cities;
+SELECT name, country FROM cities;
+SELECT name, country, population FROM cities;
+SELECT name, area FROM cities;
+SELECT name, name, country, country FROM cities;
+-- so on
